c6f4c2000eab5a51f6e04e30b039f8f7b9c6c7ec
Martin Twum Mensah <martint.mensah@gmail.com>
2019-09-10 10:45:40 +0000

    feature: Map over numbers and return a NumberButton for each button

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -15,6 +15,11 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {
+        padNumbers.map(number => (
+          <NumberButton number={number} />
+        ))
+      }
     </div>
   );
 };
