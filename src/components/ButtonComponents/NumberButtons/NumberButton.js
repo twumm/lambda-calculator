393d13daf10768cea81c19deb2708cc15829b410
Martin Twum Mensah <martint.mensah@gmail.com>
2019-09-10 17:34:57 +0000

    feature: Add className of numberButton to NumberButton main div

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import "./Number.css";
+
 const NumberButton = ({ number }) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button>{number}</button>
+      <div className='numberButton'>{number}</div>
     </>
   );
 };
