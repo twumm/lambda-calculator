289e6517248f43824fe4d7e65f006a7585449550
Martin Twum Mensah <martint.mensah@gmail.com>
2019-09-10 10:39:05 +0000

    feature: Import NumberButton

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 //import any components needed
+import NumberButton from './NumberButton';
 
 //Import your array data to from the provided data file
 import numbers from '../../../data';
