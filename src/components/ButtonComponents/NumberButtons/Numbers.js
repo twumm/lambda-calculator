d8fd6ae782bcb4adc5df49c795c46e6c32b02b43
Martin Twum Mensah <martint.mensah@gmail.com>
2019-09-10 10:36:28 +0000

    feature: Import numbers from data file Set numbers in state of Numbers component

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
 
 //Import your array data to from the provided data file
+import numbers from '../../../data';
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [ padNumbers, setPadNumbers ] = useState(numbers)
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
@@ -14,3 +17,5 @@ const Numbers = () => {
     </div>
   );
 };
+
+export default Numbers;
