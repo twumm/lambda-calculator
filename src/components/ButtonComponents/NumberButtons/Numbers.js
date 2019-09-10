6feb51390de42aeca6b09a47f6b9f89cad1aee9b
Martin Twum Mensah <martint.mensah@gmail.com>
2019-09-10 17:07:34 +0000

    chore: Correct numbers import from data file

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -4,12 +4,12 @@ import React, { useState } from "react";
 import NumberButton from './NumberButton';
 
 //Import your array data to from the provided data file
-import numbers from '../../../data';
+import { numbers } from '../../../data';
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
   const [numberState, setNumberState] = useState(numbers);
-
+  console.log(numberState);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
