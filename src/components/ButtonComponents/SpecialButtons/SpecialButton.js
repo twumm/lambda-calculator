dc52cf53fa36a1e2cede2b436b7ffb3d38d13bb0
Martin Twum Mensah <martint.mensah@gmail.com>
2019-09-10 17:56:32 +0000

    feature: Create special button Import Special css

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const SpecialButton = () => {
+import "./Special.css";
+
+const SpecialButton = ({ special }) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <div className='specialButton'>{special}</div>
     </>
   );
 };
+
+export default SpecialButton;
