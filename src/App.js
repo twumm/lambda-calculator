04ad71f8713f22905cc56214954eb1c67f0fe62d
Martin Twum Mensah <martint.mensah@gmail.com>
2019-09-10 18:15:59 +0000

    chore: Render Numbers

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-        <Numbers className="numbers" />
+        <Numbers />
       </div>
     </div>
   );
